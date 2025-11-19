253d4caed0c0e337f361542a9cb8f9e5f01bf324
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-11-19 20:18:06 +0700

    Reference Node

--- Scenes/Game/Game.cs
+++ Scenes/Game/Game.cs
@@ -3,9 +3,13 @@ using System;
 
 public partial class Game : Node2D
 {
+	[Export] private Gem _gem;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
     {
+        // Gem gem = GetNode<Gem>("Gem");
+		// gem.OnScored += OnScored;
+		_gem.OnScored += OnScored;
     }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
